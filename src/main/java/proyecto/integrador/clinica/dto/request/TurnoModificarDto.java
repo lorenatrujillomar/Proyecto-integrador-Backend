a57144b627e1e4286577278c8c40ba5f4ddc3e15
Lani23 <linalmg23@gmail.com>
2024-09-13 16:57:19 -0500

    Modificaciones en turno

--- src/main/java/proyecto/integrador/clinica/dto/request/TurnoModificarDto.java
+++ src/main/java/proyecto/integrador/clinica/dto/request/TurnoModificarDto.java
@@ -13,9 +13,9 @@ import lombok.Setter;
 @NoArgsConstructor
 public class TurnoModificarDto {
     private Integer id;
-    @NotBlank(message = "El codigo de paciente no debe ser nulo")
+    @NotNull(message = "El código de paciente no debe ser nulo")
     private Integer paciente_id;
-    @NotBlank(message = "El codigo de odontologo no debe ser nulo")
+    @NotNull(message = "El código de odontologo no debe ser nulo")
     private Integer odontologo_id;
     @NotNull(message = "La fecha no debe estar vacia")
     private String  fecha;
